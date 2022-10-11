421b7a00609aac3d3ca54bc0b3272d85f9d1ca3e
sohyunhere <shparkso162@gmail.com>
2022-10-11 16:32:46 +0900

    테스트 시나리오3

--- src/test/java/com/example/bookreview/user/service/MemberServiceTest.java
+++ src/test/java/com/example/bookreview/user/service/MemberServiceTest.java
@@ -1,36 +1,61 @@
 package com.example.bookreview.user.service;
 
+import com.example.bookreview.user.model.Member;
+import com.example.bookreview.user.model.SignupDto;
 import com.example.bookreview.user.repo.MemberRepo;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 
+@SpringBootTest
+@RunWith(MockitoJUnitRunner.class)
+@AutoConfigureMockMvc
 class MemberServiceTest {
-    @Autowired MemberService memberService;
-    @Autowired MemberRepo memberRepo;
+
+    @Mock
+    private MemberRepo memberRepo;
+
+//    @InjectMocks
+    @Autowired
+    private MemberService memberService;
+
 
     @Test
-    void loadUserByUsername() {
+    void testMock() {
+        final String email="fdfd@gmail.com";
+        given(memberRepo.findByMemberEmail(any())).willReturn(Optional.of(new Member(1L, email, "password","member-test")));
+
+        Member member = memberService.findMemberByEmail(email);
+
+        assertNotNull(member);
     }
-
     @Test
-    void join() {
-    }
+    @Transactional
+    void join() throws Exception {
+        SignupDto signupDto = new SignupDto();
+        signupDto.setPassword("yesyes");
+        signupDto.setPassword2("yesyes");
+        signupDto.setEmail("1111@gmail.com");
+        signupDto.setNickname("테스트");
+        memberService.join(signupDto);
 
-    @Test
-    void findMemberByEmail() {
-    }
-
-    @Test
-    void updateNickname() {
-    }
-
-    @Test
-    void changePassword() {
-    }
-
-    @Test
-    void changeSession() {
+        Member member = memberService.findMemberByEmail(signupDto.getEmail());
+        assertThat(signupDto.getNickname()).isEqualTo(member.getMemberNickname());
+        assertThat(signupDto.getPassword()).isEqualTo(member.getPassword());
     }
 }
